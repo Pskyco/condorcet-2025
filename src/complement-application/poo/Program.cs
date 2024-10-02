3260f9d27cb3bb3cbc1971fbed09abc1d6a34045
Ludwig Lebrun <lle@eonix.be>
2024-10-02 20:21:14 +0200

    feature: poo project

--- src/complement-application/poo/Program.cs
+++ src/complement-application/poo/Program.cs
@@ -13,3 +13,19 @@ var teacher2 = new Teacher("Ludwig", "Lebrun2");
 Console.WriteLine("Le nom de mon professeur est " + teacher.LastName + " et son prénom est " + teacher.FirstName);
 Console.WriteLine($"Le nom de mon professeur est {teacher.LastName} et son prénom est {teacher.FirstName}");
 Console.WriteLine(string.Format("Le nom de mon professeur est {0} et son prénom est {1}", teacher.LastName, teacher.FirstName));
+
+// teacher.DescribeMyself();
+// teacher.DescribeMyselfAsString();
+teacher.DescribeToConsole();
+var teacherDescription = teacher.GetDescription();
+
+// Console.WriteLine(teacher); -- same as Console.WriteLine(teacher.ToString());
+Console.WriteLine(teacher.ToString());
+
+// CTRL+R+R for renaming
+// CTRL+K+D for re-indent & clean code
+// Doc breakpoint
+// F9
+// F10
+// F11
+// SHIFT+F9
--- src/complement-application/poo/Teacher.cs
+++ src/complement-application/poo/Teacher.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace poo;
 
-public class Teacher
+public class Teacher : Object
 {
+    [Display(Name = "First name")]
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
@@ -11,9 +14,44 @@ public class Teacher
         LastName = lastName;
     }
 
-    public void DescribeMyself()
+    // public string DescribeMyself()
+    public void DescribeToConsole()
     {
         // this. is not mandatory in this case
-        Console.WriteLine($"Le nom de mon professeur est {LastName} et son prénom est {this.FirstName}");
+        Console.WriteLine(GetDescription());
     }
+
+    // public string DescribeMyselfAsString()
+    public string GetDescription()
+    {
+        return $"Le nom de mon professeur est {LastName} et son prénom est {this.FirstName}";
+    }
+
+    public override string ToString()
+    {
+        // Console.WriteLine(base.ToString());
+        return $"{nameof(FirstName)}: {FirstName}, {nameof(LastName)}: {LastName}";
+    }
+
+    // Draw(1);
+    // Draw(1, 2);
+    // public void Draw()
+    // {
+    //     
+    // }
+    //
+    // public void Draw(int a)
+    // {
+    //     
+    // }
+    //
+    // public void Draw(int a, int b)
+    // {
+    //     
+    // }
+    //
+    // public void Draw(int a, int? b = null)
+    // {
+    //     
+    // }
 }
