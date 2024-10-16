a0c971ce5b2fbd0302f0ea2a19883c84dac4b2d2
Ludwig Lebrun <lle@eonix.be>
2024-10-16 21:02:46 +0200

    feature: loop & condition projects

---
+++ src/conditions/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var userKey = Console.ReadKey();
+
+if (userKey.KeyChar == '1')
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '1'");
+
+Console.WriteLine("La suite du programme continue");
+
+if (userKey.KeyChar == '1')
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '1'");
+else
+    Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '1'");
+
+Console.WriteLine("La suite du programme continue");
+
+Console.WriteLine(userKey.KeyChar == '1'
+    ? "L'utilisateur a appuyé sur la touche '1'"
+    : "L'utilisateur a appuyé sur une autre touche que '1'");
+
+Console.WriteLine("La suite du programme continue");
+
+Console.WriteLine(userKey.KeyChar == '1'
+    ? "L'utilisateur a appuyé sur la touche '1'"
+    : userKey.KeyChar == '2'
+        ? "L'utilisateur a appuyé sur la touche '2'"
+        : userKey.KeyChar == '3'
+            ? "L'utilisateur a appuyé sur la touche '3'"
+            : "L'utilisateur a appuyé sur une autre touche que '1', '2' ou '3'");
+
+Console.WriteLine("La suite du programme continue");
+
+if (userKey.KeyChar == '1')
+{
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '1'");
+}
+else
+{
+    Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '1'");
+    if (userKey.KeyChar == '2')
+    {
+        Console.WriteLine("L'utilisateur a appuyé sur la touche '2'");
+    }
+    else
+    {
+        Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '2'");
+        if (userKey.KeyChar == '3')
+        {
+            Console.WriteLine("L'utilisateur a appuyé sur la touche '3'");
+        }
+        else
+        {
+            Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '3'");
+        }
+    }
+}
+
+Console.WriteLine("La suite du programme continue");
+
+if (userKey.KeyChar == '1')
+{
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '1'");
+}
+else if (userKey.KeyChar == '2')
+{
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '2'");
+}
+else if (userKey.KeyChar == '3')
+{
+    Console.WriteLine("L'utilisateur a appuyé sur la touche '3'");
+}
+else
+{
+    Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '1' ou '2' ou '3'");
+}
+
+Console.WriteLine("La suite du programme continue");
+
+switch (userKey.KeyChar)
+{
+    case '1':
+        Console.WriteLine("L'utilisateur a appuyé sur la touche '1'");
+        break;
+    case '2':
+        Console.WriteLine("L'utilisateur a appuyé sur la touche '2'");
+        break;
+    case '3':
+        Console.WriteLine("L'utilisateur a appuyé sur la touche '3'");
+        break;
+    case '€':
+        Console.WriteLine("L'utilisateur a appuyé sur la touche '€'");
+        break;
+    default:
+        Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '1' ou '2' ou '3'");
+        break;
+}
+
+Console.WriteLine("La suite du programme continue");
+
+switch (userKey.KeyChar)
+{
+    case '1':
+    case '3':
+        Console.WriteLine("L'utilisateur a appuyé sur l'une des touches suivantes : '1' ou '3'");
+        break;
+    case '2':
+        Console.WriteLine("L'utilisateur a appuyé sur l'une des touches suivantes : '2'");
+        break;
+    default:
+        Console.WriteLine("L'utilisateur a appuyé sur une autre touche que '1' ou '2' ou '3'");
+        break;
+}
+
+Console.WriteLine("La suite du programme continue");
---
+++ src/loops/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var integers = new List<int>()
+{
+    1,
+    45,
+    23,
+    155,
+    355,
+    3,
+    12
+};
+
+PrintWhile();
+PrintForI();
+PrintForEach();
+
+void PrintWhile()
+{
+    int i = 0;
+// while (i < integers.Count)
+    while (i <= integers.Count - 1)
+    {
+        var integer = integers[i];
+        if (integer == 355)
+        {
+            break; // La boucle est totalement arrêtée
+        }
+        else if (integer == 23)
+        {
+            i++;
+            continue; // "Passe à l'itération suivante"
+        }
+        else
+        {
+            Console.WriteLine($"[1] Mon entier à l'index {i} est {integer}");
+        }
+
+        i++;
+    }
+}
+
+void PrintForI()
+{
+// for (int i = 0; i < 7; i++)
+// for (int i = 0; i <= 7; i++) // will throw a 'ArgumentOutOfRangeException : Index was out of range'
+// for (int i = 0; i <= 6; i++)
+    for (int i = 0; i < integers.Count; i++)
+    {
+        var integer = integers[i];
+        if (integer == 355)
+        {
+            break; // La boucle est totalement arrêtée
+        }
+        else if (integer == 23)
+        {
+            continue; // "Passe à l'itération suivante"
+        }
+        else
+        {
+            Console.WriteLine($"[1] Mon entier à l'index {i} est {integer}");
+        }
+    }
+
+    for (int i = 2; i < integers.Count; i++)
+    {
+        Console.WriteLine($"[2] Mon entier à l'index {i} est {integers[i]}");
+    }
+
+    for (int i = 0; i < integers.Count - 2; i++)
+    {
+        Console.WriteLine($"[3] Mon entier à l'index {i} est {integers[i]}");
+    }
+
+    for (int i = integers.Count - 1; i >= 0; i--)
+    {
+        Console.WriteLine($"[4] Mon entier à l'index {i} est {integers[i]}");
+    }
+}
+
+void PrintForEach()
+{
+    int index = 0;
+    foreach (int integer in integers)
+    {
+        if (integer == 355)
+        {
+            break; // La boucle est totalement arrêtée
+        }
+        else if (integer == 23)
+        {
+            continue; // "Passe à l'itération suivante"
+        }
+        else
+        {
+            Console.WriteLine($"[1] Mon entier à l'index {index} est {integer}");
+            // index = index + 1;
+            // index += 1;
+            index++;
+        }
+    }
+
+    Console.WriteLine("=================================");
+
+    index = 0;
+    foreach (int integer in integers)
+    {
+        if (integer == 355)
+            break; // La boucle est totalement arrêtée
+
+        if (integer == 23)
+            continue; // "Passe à l'itération suivante"
+
+        Console.WriteLine($"[2] Mon entier à l'index {index} est {integer}");
+        // index = index + 1;
+        // index += 1;
+        index++;
+    }
+
+    Console.WriteLine("=================================");
+
+    index = -1;
+    foreach (int integer in integers)
+    {
+        index++;
+
+        if (index < 2)
+            continue;
+
+        Console.WriteLine($"[3] Mon entier à l'index {index} est {integer}");
+    }
+
+    Console.WriteLine("La suite du programme continue");
+}
